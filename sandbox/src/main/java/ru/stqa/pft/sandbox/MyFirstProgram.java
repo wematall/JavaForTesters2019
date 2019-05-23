845befdd1ba4b928d9d71e53066f65f4dde7e05f
wematall <wematall@gmail.com>
2019-05-23 19:09:07 +0300

    сделал изменения в методе main() где создаю объекты для вычисления площади квадрата и прямоугольника

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -8,12 +8,11 @@ public class MyFirstProgram {
     hello("user");
     hello("Andrey");
 
-    double l = 5;
-    System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
+    Square s = new Square(5);
+    System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
 
-    double a = 4;
-    double b = 6;
-    System.out.println("Площадь прямоугольника со сторонами "+ a + " и " + b + " = " + area(a, b));
+    Rectangle r = new Rectangle(4, 6);
+    System.out.println("Площадь прямоугольника со сторонами "+ r.a + " и " + r.b + " = " + r.area());
 
   }
 
@@ -21,14 +20,6 @@ public class MyFirstProgram {
 
     System.out.println("Hello, " + somebody + "!");
 
-  }
-
-  public static double area (double len) {
-    return len * len;
-  }
-
-  public static double area(double a, double b) {
-    return a * b;
   }
 
 }
