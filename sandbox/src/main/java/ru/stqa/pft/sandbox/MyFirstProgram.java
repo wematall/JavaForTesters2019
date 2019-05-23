0b0d90c62d07acf3beaee1b7ef6cf437ede560ed
wematall <wematall@gmail.com>
2019-05-23 21:53:43 +0300

    изменил имена переменных x, y

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -23,8 +23,8 @@ public class MyFirstProgram {
   }
 
   public static double distance(Point p1, Point p2) {
-    double a = p1.x - p2.x;
-    double b = p1.y - p2.y;
+    double a = p1.x1 - p2.x1;
+    double b = p1.y1 - p2.y1;
     double result = Math.sqrt((a * a) + (b * b));
 
     return result;
--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -2,11 +2,22 @@ package ru.stqa.pft.sandbox;
 
 public class Point {
 
-	double x;
-	double y;
+	double x1;
+	double x2;
+	double y1;
+	double y2;
 
-	public Point(double x, double y) {
-		this.x = x;
-		this.y = y;
+	public Point(double x1, double y1) {
+		this.x1 = x1;
+		this.y1 = y1;
 	}
+
+	public Point(double x1, double y1, double x2, double y2) {
+		this.x1 = x1;
+		this.x2 = x2;
+		this.y1 = y1;
+		this.y2 = y2;
+	}
+
+
 }
