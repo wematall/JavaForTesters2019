8d04e4eb4a1a5010c848ce645671b06ef339a9f4
wematall <wematall@gmail.com>
2019-05-23 22:18:14 +0300

    изменил метод main() добавил примеры использования метода distance() из класса Point. Теперь все вычисления расстояния между точками происходят в классе Point, в методе distance()

--- sandbox/src/main/java/ru/stqa/pft/sandbox/CountDistance.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/CountDistance.java
@@ -4,18 +4,14 @@ public class CountDistance {
 
 	public static void main(String[] args) {
 
-		Point p1 = new Point(2, 1);
-		Point p2 = new Point(4, 6);
+		Point d1 = new Point(2, 4, 3, 6);
+		Point d2 = new Point(-2, 4, -3, 6);
+		Point d3 = new Point(0, 0, 0, 0);
 
-		System.out.println(distance(p1, p2));
+		System.out.println(d1.distance());
+		System.out.println(d2.distance());
+		System.out.println(d3.distance());
 
 	}
 
-	public static double distance(Point p1, Point p2) {
-		double a = p1.x1 - p2.x1;
-		double b = p1.y1 - p2.y1;
-		double result = Math.sqrt((a * a) + (b * b));
-
-		return result;
-	}
 }
