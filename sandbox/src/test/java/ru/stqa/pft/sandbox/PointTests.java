3f36a7d5d2e0b02ee00181e1e8f2b99c7b19796d
wematall <wematall@gmail.com>
2019-05-24 23:19:15 +0300

    Внес изменения в тесты PointTests с учетом изменений в классе Point. Изменил имена переменных в классе MyFirstProgram

--- sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
@@ -7,21 +7,24 @@ public class PointTests {
 
 	@Test
 	public void testDistance1() {
-		Point p = new Point(2, 4, 3, 6);
+		Point p1 = new Point(2, 4);
+		Point p2 = new Point(3, 6);
 
-		Assert.assertEquals(p.distance(), 2.23606797749979);
+		Assert.assertEquals(p1.getdistance(p2), 2.23606797749979);
 	}
 
 	@Test
 	public void testDistance2() {
-		Point p = new Point(-2, 4, -3, 6);
-		Assert.assertEquals(p.distance(), 2.23606797749979);
+		Point p1 = new Point(-2, 4);
+		Point p2 = new Point(-3, 6);
+		Assert.assertEquals(p1.getdistance(p2), 2.23606797749979);
 	}
 
 	@Test
 	public void testDistance3() {
-		Point p = new Point(0, 0, 0, 0);
-		Assert.assertEquals(p.distance(), 0.0);
+		Point p1 = new Point(0, 0);
+		Point p2 = new Point(0, 0);
+		Assert.assertEquals(p1.getdistance(p2), 0.0);
 	}
 
 
