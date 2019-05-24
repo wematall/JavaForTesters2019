ac02ea606f289cc272f4477f3be220a80d71840d
wematall <wematall@gmail.com>
2019-05-24 23:00:30 +0300

    класс Point переделал, на вход принимает одну точку (x,y). Реализовал метод getdistance() вычисляющий расстояние между точками.

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -2,30 +2,26 @@ package ru.stqa.pft.sandbox;
 
 public class Point {
 
-	double x1;
-	double x2;
-	double y1;
-	double y2;
+	double x;
+	double y;
 
-	public Point(double x1, double y1) {
-		this.x1 = x1;
-		this.y1 = y1;
+
+	public Point(double x, double y) {
+		this.x = x;
+		this.y = y;
 	}
 
-	public Point(double x1, double y1, double x2, double y2) {
-		this.x1 = x1;
-		this.x2 = x2;
-		this.y1 = y1;
-		this.y2 = y2;
+	public double getdistance(double x, double y) {
+		double sx = this.x - x;
+		double sy = this.y - y;
+
+		return Math.sqrt((sx*sx) + (sy*sy));
 	}
 
-	public double distance() {
-		double a = x1 - x2;
-		double b = y1 - y2;
-		double result = Math.sqrt((a*a) + (b*b));
-
-		return result;
+	public double getdistance(Point p) {
+		return getdistance(p.x, p.y);
 	}
+
 
 
 }
