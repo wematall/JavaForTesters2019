969f3b49d508d27954f5829093fa9aef02214216
wematall <wematall@gmail.com>
2019-05-25 18:45:39 +0300

    изменил один из методов. Теперь в кач-ве параметра принимает объект

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactCreationTests.java
@@ -15,7 +15,7 @@ public class ContactCreationTests {
     driver = new FirefoxDriver();
     driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     driver.get("http://192.168.63.138/addressbook/");
-    
+
     login("admin", "secret");
   }
 
@@ -33,7 +33,7 @@ public class ContactCreationTests {
   public void testContactCreation() throws Exception {
 
     initContactCreation();
-    fillContactForm("testName", "testMiddleName", "testLastName", "testNickname", "mr", "testCompany", "testCity, st. testStreet, 11", "11111111111", "22222222222", "22222222222", "33333333333", "testmail@mail.test", "testmai2@mail.test", "testmail3@mail.test", "www.somewebpage.test", "7", "August", "1988", "11", "December", "2011", "testCity2, st. testStreet, 111", "internet virtual home", "some notes, some comments here");
+    fillContactForm(new ContactData("testMiddleName", "testLastName", "testNickname", "mr", "testCompany", "testCity, st. testStreet, 11", "11111111111", "22222222222", "22222222222", "33333333333", "testmail@mail.test", "testName", "testmai2@mail.test", "testmail3@mail.test", "www.somewebpage.test", "7", "August", "1988", "11", "December", "2011", "testCity2, st. testStreet, 111", "internet virtual home", "some notes, some comments here"));
     submitContactForm();
     logout();
 
@@ -47,75 +47,75 @@ public class ContactCreationTests {
     driver.findElement(By.xpath("(//input[@name='submit'])[2]")).click();
   }
 
-  private void fillContactForm(String firstName, String middleName, String lastName, String nickName, String title, String companyName, String address, String phoneHome, String phoneMobile, String phoneWork, String fax, String email, String email2, String email3, String webPage, String birthDay, String birthMonth, String birthYear, String annyversaryDay, String annyversaryMonth, String annyversaryYear, String address2, String phoneHome2, String notes) {
+  private void fillContactForm(ContactData contactData) {
     driver.findElement(By.name("firstname")).click();
     driver.findElement(By.name("firstname")).clear();
-    driver.findElement(By.name("firstname")).sendKeys(firstName);
+    driver.findElement(By.name("firstname")).sendKeys(contactData.getFirstName());
     driver.findElement(By.name("middlename")).click();
     driver.findElement(By.name("middlename")).clear();
-    driver.findElement(By.name("middlename")).sendKeys(middleName);
+    driver.findElement(By.name("middlename")).sendKeys(contactData.getMiddleName());
     driver.findElement(By.name("lastname")).click();
     driver.findElement(By.name("lastname")).clear();
-    driver.findElement(By.name("lastname")).sendKeys(lastName);
+    driver.findElement(By.name("lastname")).sendKeys(contactData.getLastName());
     driver.findElement(By.name("nickname")).click();
     driver.findElement(By.name("nickname")).clear();
-    driver.findElement(By.name("nickname")).sendKeys(nickName);
+    driver.findElement(By.name("nickname")).sendKeys(contactData.getNickName());
     driver.findElement(By.name("title")).click();
     driver.findElement(By.name("title")).clear();
-    driver.findElement(By.name("title")).sendKeys(title);
+    driver.findElement(By.name("title")).sendKeys(contactData.getTitle());
     driver.findElement(By.name("company")).click();
     driver.findElement(By.name("company")).clear();
-    driver.findElement(By.name("company")).sendKeys(companyName);
+    driver.findElement(By.name("company")).sendKeys(contactData.getCompanyName());
     driver.findElement(By.name("address")).click();
     driver.findElement(By.name("address")).clear();
-    driver.findElement(By.name("address")).sendKeys(address);
+    driver.findElement(By.name("address")).sendKeys(contactData.getAddress());
     driver.findElement(By.name("home")).click();
     driver.findElement(By.name("home")).clear();
-    driver.findElement(By.name("home")).sendKeys(phoneHome);
+    driver.findElement(By.name("home")).sendKeys(contactData.getPhoneHome());
     driver.findElement(By.name("mobile")).click();
     driver.findElement(By.name("mobile")).clear();
-    driver.findElement(By.name("mobile")).sendKeys(phoneMobile);
+    driver.findElement(By.name("mobile")).sendKeys(contactData.getPhoneMobile());
     driver.findElement(By.name("work")).click();
     driver.findElement(By.name("work")).clear();
-    driver.findElement(By.name("work")).sendKeys(phoneWork);
+    driver.findElement(By.name("work")).sendKeys(contactData.getPhoneWork());
     driver.findElement(By.name("fax")).click();
     driver.findElement(By.name("fax")).clear();
-    driver.findElement(By.name("fax")).sendKeys(fax);
+    driver.findElement(By.name("fax")).sendKeys(contactData.getFax());
     driver.findElement(By.name("email")).click();
     driver.findElement(By.name("email")).clear();
-    driver.findElement(By.name("email")).sendKeys(email);
+    driver.findElement(By.name("email")).sendKeys(contactData.getEmail());
     driver.findElement(By.name("email2")).click();
     driver.findElement(By.name("email2")).clear();
-    driver.findElement(By.name("email2")).sendKeys(email2);
+    driver.findElement(By.name("email2")).sendKeys(contactData.getEmail2());
     driver.findElement(By.name("email3")).click();
     driver.findElement(By.name("email3")).clear();
-    driver.findElement(By.name("email3")).sendKeys(email3);
+    driver.findElement(By.name("email3")).sendKeys(contactData.getEmail3());
     driver.findElement(By.name("homepage")).click();
     driver.findElement(By.name("homepage")).clear();
-    driver.findElement(By.name("homepage")).sendKeys(webPage);
-    new Select(driver.findElement(By.name("bday"))).selectByVisibleText(birthDay);
+    driver.findElement(By.name("homepage")).sendKeys(contactData.getWebPage());
+    new Select(driver.findElement(By.name("bday"))).selectByVisibleText(contactData.getBirthDay());
     driver.findElement(By.name("bday")).click();
-    new Select(driver.findElement(By.name("bmonth"))).selectByVisibleText(birthMonth);
+    new Select(driver.findElement(By.name("bmonth"))).selectByVisibleText(contactData.getBirthMonth());
     driver.findElement(By.name("bmonth")).click();
     driver.findElement(By.name("byear")).click();
     driver.findElement(By.name("byear")).clear();
-    driver.findElement(By.name("byear")).sendKeys(birthYear);
-    new Select(driver.findElement(By.name("aday"))).selectByVisibleText(annyversaryDay);
+    driver.findElement(By.name("byear")).sendKeys(contactData.getBirthYear());
+    new Select(driver.findElement(By.name("aday"))).selectByVisibleText(contactData.getAnnyversaryDay());
     driver.findElement(By.name("aday")).click();
-    new Select(driver.findElement(By.name("amonth"))).selectByVisibleText(annyversaryMonth);
+    new Select(driver.findElement(By.name("amonth"))).selectByVisibleText(contactData.getAnnyversaryMonth());
     driver.findElement(By.name("amonth")).click();
     driver.findElement(By.name("ayear")).click();
     driver.findElement(By.name("ayear")).clear();
-    driver.findElement(By.name("ayear")).sendKeys(annyversaryYear);
+    driver.findElement(By.name("ayear")).sendKeys(contactData.getAnnyversaryYear());
     driver.findElement(By.name("address2")).click();
     driver.findElement(By.name("address2")).clear();
-    driver.findElement(By.name("address2")).sendKeys(address2);
+    driver.findElement(By.name("address2")).sendKeys(contactData.getAddress2());
     driver.findElement(By.name("phone2")).click();
     driver.findElement(By.name("phone2")).clear();
-    driver.findElement(By.name("phone2")).sendKeys(phoneHome2);
+    driver.findElement(By.name("phone2")).sendKeys(contactData.getPhoneHome2());
     driver.findElement(By.name("notes")).click();
     driver.findElement(By.name("notes")).clear();
-    driver.findElement(By.name("notes")).sendKeys(notes);
+    driver.findElement(By.name("notes")).sendKeys(contactData.getNotes());
   }
 
   private void initContactCreation() {
