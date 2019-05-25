fb6aea721f49c027be1aa76063318490b3f41db6
wematall <wematall@gmail.com>
2019-05-25 16:40:36 +0300

    Изменил метод заполнения полей формы. Теперь метод принимает объект в качестве параметров

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
@@ -31,7 +31,7 @@ public class GroupCreationTests {
   public void testGroupCreation() throws Exception {
     gotoGroupPage();
     initGroupCreation();
-    fillGroupForm("test 1", "test header logo", "test footer comment");
+    fillGroupForm(new GroupData("test 1", "test header logo", "test footer comment"));
     submitGroupCreation();
     gotoGroupPage();
     logout();
@@ -45,16 +45,16 @@ public class GroupCreationTests {
     wd.findElement(By.name("submit")).click();
   }
 
-  private void fillGroupForm(String groupName, String groupHeader, String groupFooter) {
+  private void fillGroupForm(GroupData groupData) {
     wd.findElement(By.name("group_name")).click();
     wd.findElement(By.name("group_name")).clear();
-    wd.findElement(By.name("group_name")).sendKeys(groupName);
+    wd.findElement(By.name("group_name")).sendKeys(groupData.getGroupName());
     wd.findElement(By.name("group_header")).click();
     wd.findElement(By.name("group_header")).clear();
-    wd.findElement(By.name("group_header")).sendKeys(groupHeader);
+    wd.findElement(By.name("group_header")).sendKeys(groupData.getGroupHeader());
     wd.findElement(By.name("group_footer")).click();
     wd.findElement(By.name("group_footer")).clear();
-    wd.findElement(By.name("group_footer")).sendKeys(groupFooter);
+    wd.findElement(By.name("group_footer")).sendKeys(groupData.getGroupFooter());
   }
 
   private void initGroupCreation() {
