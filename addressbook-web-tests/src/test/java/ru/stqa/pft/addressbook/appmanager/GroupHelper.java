2b7e8c093d51709615f273878fdbb934f2a86291
wematall <wematall@gmail.com>
2019-06-04 22:09:49 +0300

    создал класс помощник GroupHelper

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
@@ -0,0 +1,42 @@
+package ru.stqa.pft.addressbook.appmanager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import ru.stqa.pft.addressbook.model.GroupData;
+
+public class GroupHelper {
+
+	private FirefoxDriver wd;
+
+	public GroupHelper(FirefoxDriver wd) {
+		this.wd = wd;
+	}
+
+	public void submitGroupCreation() {
+		wd.findElement(By.name("submit")).click();
+	}
+
+	public void fillGroupForm(GroupData groupData) {
+		wd.findElement(By.name("group_name")).click();
+		wd.findElement(By.name("group_name")).clear();
+		wd.findElement(By.name("group_name")).sendKeys(groupData.getGroupName());
+		wd.findElement(By.name("group_header")).click();
+		wd.findElement(By.name("group_header")).clear();
+		wd.findElement(By.name("group_header")).sendKeys(groupData.getGroupHeader());
+		wd.findElement(By.name("group_footer")).click();
+		wd.findElement(By.name("group_footer")).clear();
+		wd.findElement(By.name("group_footer")).sendKeys(groupData.getGroupFooter());
+	}
+
+	public void initGroupCreation() {
+		wd.findElement(By.name("new")).click();
+	}
+
+	public void deleteSelectedGroup() {
+		wd.findElement(By.name("delete")).click();
+	}
+
+	public void selectGroup() {
+		wd.findElement(By.name("selected[]")).click();
+	}
+}
