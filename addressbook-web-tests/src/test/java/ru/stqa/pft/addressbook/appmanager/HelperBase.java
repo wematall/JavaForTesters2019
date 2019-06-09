83afd095a6c0c20402483bbd5cb252debf000e6c
wematall <wematall@gmail.com>
2019-06-09 19:32:44 +0300

    создал класс HelperBase перенес в него общие методы из классов помощников

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/HelperBase.java
@@ -0,0 +1,44 @@
+package ru.stqa.pft.addressbook.appmanager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class HelperBase {
+	protected FirefoxDriver wd;
+
+	public HelperBase(FirefoxDriver wd) {
+		this.wd = wd;
+	}
+
+	protected void click(By locator) {
+		wd.findElement(locator).click();
+	}
+
+	protected void type(By locator, String text) {
+		click(locator);
+		wd.findElement(locator).clear();
+		wd.findElement(locator).sendKeys(text);
+	}
+
+	private boolean isAlertPresent() {
+		try {
+			wd.switchTo().alert();
+			return true;
+		} catch (NoAlertPresentException e) {
+			return false;
+		}
+	}
+
+	private boolean isElementPresent(By by) {
+		try {
+			wd.findElement(by);
+			return true;
+		} catch (NoSuchElementException e) {
+			return false;
+		}
+	}
+
+
+}
