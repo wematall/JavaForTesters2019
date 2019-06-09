e9d0bcf179f5aa9f1b6bcc644b919df33b37e047
wematall <wematall@gmail.com>
2019-06-09 19:36:13 +0300

    из классов помощников перенес общие методы в класс HelperBase

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -1,10 +1,6 @@
 package ru.stqa.pft.addressbook.appmanager;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.NoAlertPresentException;
-import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.firefox.FirefoxDriver;
-
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
@@ -29,27 +25,8 @@ public class ApplicationManager {
 		sessionHelper.login("admin", "secret");
 	}
 
-
 	public void stop() {
 		wd.quit();
-	}
-
-	private boolean isElementPresent(By by) {
-		try {
-			wd.findElement(by);
-			return true;
-		} catch (NoSuchElementException e) {
-			return false;
-		}
-	}
-
-	private boolean isAlertPresent() {
-		try {
-			wd.switchTo().alert();
-			return true;
-		} catch (NoAlertPresentException e) {
-			return false;
-		}
 	}
 
 
