3b5ef1184bd9088493db9c16d6a20e8a32878dc9
wematall <wematall@gmail.com>
2019-06-09 21:19:42 +0300

    поменял тестовые данные для поля firstName

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -12,7 +12,7 @@ public class ContactCreationTests extends TestBase {
   public void testContactCreation() throws Exception {
 
     app.contactHelper.initContactCreation();
-    app.contactHelper.fillContactForm(new ContactData("testMiddleName", "testLastName", "testNickname", "mr", "testCompany", "testCity, st. testStreet, 11", "11111111111", "22222222222", "22222222222", "33333333333", "testmail@mail.test", "testName", "testmai2@mail.test", "testmail3@mail.test", "www.somewebpage.test", "7", "August", "1988", "11", "December", "2011", "testCity2, st. testStreet, 111", "internet virtual home", "some notes, some comments here"));
+    app.contactHelper.fillContactForm(new ContactData("testMiddleName", "testLastName", "testNickname", "mr", "testCompany", "testCity, st. testStreet, 11", "11111111111", "22222222222", "22222222222", "33333333333", "testmail@mail.test", "testFirstName", "testmai2@mail.test", "testmail3@mail.test", "www.somewebpage.test", "7", "August", "1988", "11", "December", "2011", "testCity2, st. testStreet, 111", "internet virtual home", "some notes, some comments here"));
     app.contactHelper.submitContactForm();
     app.getSessionHelper().logout();
 
