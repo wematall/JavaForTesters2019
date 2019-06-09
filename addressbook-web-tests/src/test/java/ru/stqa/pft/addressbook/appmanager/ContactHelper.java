01d2d93fb02440e56a2af12a6139fb1ed5ee42de
wematall <wematall@gmail.com>
2019-06-09 21:22:36 +0300

    модифицировал помощник для использования родительского класса HelperBase и использую метод type()

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -5,91 +5,59 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
 import ru.stqa.pft.addressbook.model.ContactData;
 
-public class ContactHelper {
-
-	private FirefoxDriver wd;
+public class ContactHelper extends HelperBase{
 
 	public ContactHelper (FirefoxDriver wd) {
-		this.wd =wd;
+		super(wd);
 	}
 
 
 	public void submitContactForm() {
-		wd.findElement(By.xpath("(//input[@name='submit'])[2]")).click();
+		click(By.xpath("(//input[@name='submit'])[2]"));
 	}
 
 	public void initContactCreation() {
-		wd.findElement(By.linkText("add new")).click();
+		click(By.linkText("add new"));
 	}
 
 	public void fillContactForm(ContactData contactData) {
-		wd.findElement(By.name("firstname")).click();
-		wd.findElement(By.name("firstname")).clear();
-		wd.findElement(By.name("firstname")).sendKeys(contactData.getFirstName());
-		wd.findElement(By.name("middlename")).click();
-		wd.findElement(By.name("middlename")).clear();
-		wd.findElement(By.name("middlename")).sendKeys(contactData.getMiddleName());
-		wd.findElement(By.name("lastname")).click();
-		wd.findElement(By.name("lastname")).clear();
-		wd.findElement(By.name("lastname")).sendKeys(contactData.getLastName());
-		wd.findElement(By.name("nickname")).click();
-		wd.findElement(By.name("nickname")).clear();
-		wd.findElement(By.name("nickname")).sendKeys(contactData.getNickName());
-		wd.findElement(By.name("title")).click();
-		wd.findElement(By.name("title")).clear();
-		wd.findElement(By.name("title")).sendKeys(contactData.getTitle());
-		wd.findElement(By.name("company")).click();
-		wd.findElement(By.name("company")).clear();
-		wd.findElement(By.name("company")).sendKeys(contactData.getCompanyName());
-		wd.findElement(By.name("address")).click();
-		wd.findElement(By.name("address")).clear();
-		wd.findElement(By.name("address")).sendKeys(contactData.getAddress());
-		wd.findElement(By.name("home")).click();
-		wd.findElement(By.name("home")).clear();
-		wd.findElement(By.name("home")).sendKeys(contactData.getPhoneHome());
-		wd.findElement(By.name("mobile")).click();
-		wd.findElement(By.name("mobile")).clear();
-		wd.findElement(By.name("mobile")).sendKeys(contactData.getPhoneMobile());
-		wd.findElement(By.name("work")).click();
-		wd.findElement(By.name("work")).clear();
-		wd.findElement(By.name("work")).sendKeys(contactData.getPhoneWork());
-		wd.findElement(By.name("fax")).click();
-		wd.findElement(By.name("fax")).clear();
-		wd.findElement(By.name("fax")).sendKeys(contactData.getFax());
-		wd.findElement(By.name("email")).click();
-		wd.findElement(By.name("email")).clear();
-		wd.findElement(By.name("email")).sendKeys(contactData.getEmail());
-		wd.findElement(By.name("email2")).click();
-		wd.findElement(By.name("email2")).clear();
-		wd.findElement(By.name("email2")).sendKeys(contactData.getEmail2());
-		wd.findElement(By.name("email3")).click();
-		wd.findElement(By.name("email3")).clear();
-		wd.findElement(By.name("email3")).sendKeys(contactData.getEmail3());
-		wd.findElement(By.name("homepage")).click();
-		wd.findElement(By.name("homepage")).clear();
-		wd.findElement(By.name("homepage")).sendKeys(contactData.getWebPage());
+		type(By.name("firstname"), contactData.getFirstName());
+		type(By.name("middlename"), contactData.getMiddleName());
+		type(By.name("lastname"), contactData.getLastName());
+		type(By.name("nickname"), contactData.getNickName());
+		type(By.name("title"), contactData.getTitle());
+		type(By.name("company"), contactData.getCompanyName());
+		type(By.name("address"), contactData.getAddress());
+		type(By.name("home"), contactData.getPhoneHome());
+		type(By.name("mobile"), contactData.getPhoneMobile());
+		type(By.name("work"), contactData.getPhoneWork());
+		type(By.name("fax"), contactData.getFax());
+		type(By.name("email"), contactData.getEmail());
+		type(By.name("email2"), contactData.getEmail2());
+		type(By.name("email3"), contactData.getEmail3());
+		type(By.name("homepage"), contactData.getWebPage());
+
+		//пока не знаю что с этим делать
+		//по идее нужно создание этих объектов
+		//перенести в другое место
 		new Select(wd.findElement(By.name("bday"))).selectByVisibleText(contactData.getBirthDay());
 		wd.findElement(By.name("bday")).click();
+
 		new Select(wd.findElement(By.name("bmonth"))).selectByVisibleText(contactData.getBirthMonth());
 		wd.findElement(By.name("bmonth")).click();
-		wd.findElement(By.name("byear")).click();
-		wd.findElement(By.name("byear")).clear();
-		wd.findElement(By.name("byear")).sendKeys(contactData.getBirthYear());
+
+		type(By.name("byear"), contactData.getBirthYear());
+
 		new Select(wd.findElement(By.name("aday"))).selectByVisibleText(contactData.getAnnyversaryDay());
 		wd.findElement(By.name("aday")).click();
+
 		new Select(wd.findElement(By.name("amonth"))).selectByVisibleText(contactData.getAnnyversaryMonth());
 		wd.findElement(By.name("amonth")).click();
-		wd.findElement(By.name("ayear")).click();
-		wd.findElement(By.name("ayear")).clear();
-		wd.findElement(By.name("ayear")).sendKeys(contactData.getAnnyversaryYear());
-		wd.findElement(By.name("address2")).click();
-		wd.findElement(By.name("address2")).clear();
-		wd.findElement(By.name("address2")).sendKeys(contactData.getAddress2());
-		wd.findElement(By.name("phone2")).click();
-		wd.findElement(By.name("phone2")).clear();
-		wd.findElement(By.name("phone2")).sendKeys(contactData.getPhoneHome2());
-		wd.findElement(By.name("notes")).click();
-		wd.findElement(By.name("notes")).clear();
-		wd.findElement(By.name("notes")).sendKeys(contactData.getNotes());
+
+		type(By.name("ayear"), contactData.getAnnyversaryYear());
+		type(By.name("address2"), contactData.getAddress2());
+		type(By.name("phone2"), contactData.getPhoneHome2());
+		type(By.name("notes"), contactData.getNotes());
+
 	}
 }
