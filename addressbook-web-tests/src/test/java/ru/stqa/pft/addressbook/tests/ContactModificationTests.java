16a53af641068e935bbbb071140d5cf3280248e8
wematall <wematall@gmail.com>
2019-06-10 21:14:22 +0300

    добавил тест класс для модификации контакта

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -0,0 +1,17 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.ContactData;
+
+public class ContactModificationTests extends TestBase {
+
+	@Test
+	public void testContactModification () {
+		app.getNavigationHelper().goToContactPage();
+		app.getContactHelper().selectContact();
+		app.getContactHelper().initContactModification();
+		app.getContactHelper().fillContactForm(new ContactData("testNewMiddleName", "testNewLastName", "testNewNickname", "mr", "testCompany", "testCity, st. testStreet, 11", "11111111111", "22222222222", "22222222222", "33333333333", "testmail@mail.test", "testFirstName", "testmai2@mail.test", "testmail3@mail.test", "www.somewebpage.test", "7", "August", "1988", "11", "December", "2011", "testCity2, st. testStreet, 111", "internet virtual home", "some notes, some comments here"));
+		app.getContactHelper().submitContactModification();
+		app.getNavigationHelper().goToContactPage();
+	}
+}
