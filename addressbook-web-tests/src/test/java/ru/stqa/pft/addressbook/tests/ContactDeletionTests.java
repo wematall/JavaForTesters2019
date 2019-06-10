2e89af5fe0058d482179c09b18f2213b51c1ec0f
wematall <wematall@gmail.com>
2019-06-10 21:42:51 +0300

    Добавил тест класс для удаления контакта

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
@@ -0,0 +1,15 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.Test;
+
+public class ContactDeletionTests extends TestBase {
+
+	@Test
+	public void testContactDeletion () {
+		app.getNavigationHelper().goToContactPage();
+		app.getContactHelper().selectContact();
+		app.getContactHelper().deleteSelectedContact();
+		app.getContactHelper().submitContactDeletion();
+		app.getNavigationHelper().goToContactPage();
+	}
+}
