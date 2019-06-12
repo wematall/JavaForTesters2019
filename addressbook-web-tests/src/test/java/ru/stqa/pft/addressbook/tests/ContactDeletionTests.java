12d845d078c41ad0f5bcab8035c9c6f5c9f415c4
wematall <wematall@gmail.com>
2019-06-12 18:43:45 +0300

    добавил проверку что контакт существует и если нет то предварительно создается

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
@@ -1,12 +1,18 @@
 package ru.stqa.pft.addressbook.tests;
 
 import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.ContactData;
 
 public class ContactDeletionTests extends TestBase {
 
 	@Test
 	public void testContactDeletion () {
 		app.getNavigationHelper().goToContactPage();
+
+		if (! app.getContactHelper().isThereContact()) {
+			app.getContactHelper().createContact(new ContactData("testMiddleName", "testLastName", "testNickname", "mr", "testCompany", "testCity, st. testStreet, 11", "11111111111", "22222222222", "22222222222", "33333333333", "testmail@mail.test", "testFirstName", "testmai2@mail.test", "testmail3@mail.test", "www.somewebpage.test", "7", "August", "1988", "11", "December", "2011", "test 1","testCity2, st. testStreet, 111", "internet virtual home", "some notes, some comments here"), true);
+			app.getNavigationHelper().goToContactPage();
+		}
 		app.getContactHelper().selectContact();
 		app.getContactHelper().deleteSelectedContact();
 		app.getContactHelper().submitContactDeletion();
