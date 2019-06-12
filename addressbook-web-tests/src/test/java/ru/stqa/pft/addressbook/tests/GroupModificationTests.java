59f82352b843e69fbf96aa0769aaabf11cd2a2ac
wematall <wematall@gmail.com>
2019-06-12 18:14:40 +0300

    добавил проверку существования группы

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -8,9 +8,15 @@ public class GroupModificationTests extends TestBase {
 	@Test
 	public void testGroupModification () {
 		app.getNavigationHelper().gotoGroupPage();
+
+		if (! app.getGroupHelper().isThereGroup()) {
+			app.getGroupHelper().createGroup(new GroupData("test 2", null, null));
+			app.getNavigationHelper().gotoGroupPage();
+		}
+
 		app.getGroupHelper().selectGroup();
 		app.getGroupHelper().initGroupModification();
-		app.getGroupHelper().fillGroupForm(new GroupData("changedName", "changedHeader", "changedFooter"));
+		app.getGroupHelper().fillGroupForm(new GroupData("changedGroupName", "changedHeader", "changedFooter"));
 		app.getGroupHelper().submitGroupModification();
 		app.getNavigationHelper().gotoGroupPage();
 
