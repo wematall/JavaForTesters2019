5075c7cf1c4d79a5cc0860b8aff0c774ac0008cd
wematall <wematall@gmail.com>
2019-06-12 11:28:22 +0300

    модифицировал класс, чтобы тип браузера передавался в качестве параметра

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -1,14 +1,16 @@
 package ru.stqa.pft.addressbook.tests;
 
 
+import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
 
 
+
 public class TestBase {
 
-	public ApplicationManager app = new ApplicationManager();
+	public ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
 
 	@BeforeMethod(alwaysRun = true)
 	public void setUp() throws Exception {
