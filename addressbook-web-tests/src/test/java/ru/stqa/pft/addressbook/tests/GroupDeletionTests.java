71c0ed5cfc1ad6016caf16c151e0859c4aeccd7d
wematall <wematall@gmail.com>
2019-06-12 18:13:33 +0300

    добавил проверку существования группы перед удалением, и если группы нет то создаем

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
@@ -1,6 +1,7 @@
 package ru.stqa.pft.addressbook.tests;
 
 import org.testng.annotations.*;
+import ru.stqa.pft.addressbook.model.GroupData;
 
 
 public class GroupDeletionTests extends TestBase {
@@ -9,6 +10,12 @@ public class GroupDeletionTests extends TestBase {
   @Test
   public void testGroupDeletion() throws Exception {
     app.getNavigationHelper().gotoGroupPage();
+
+    if (! app.getGroupHelper().isThereGroup()) {
+      app.getGroupHelper().createGroup(new GroupData("test 1", null, null));
+      app.getNavigationHelper().gotoGroupPage();
+    }
+
     app.getGroupHelper().selectGroup();
     app.getGroupHelper().deleteSelectedGroup();
     app.getNavigationHelper().gotoGroupPage();
