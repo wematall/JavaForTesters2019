5514095fc5d756d7b7e09b428e540065d7aa4240
wematall <wematall@gmail.com>
2019-06-12 13:39:09 +0300

    изменил тест, чтобы использовать возможность оставить значение поля по умолчанию

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -11,7 +11,7 @@ public class GroupCreationTests extends TestBase {
   public void testGroupCreation() throws Exception {
     app.getNavigationHelper().gotoGroupPage();
     app.getGroupHelper().initGroupCreation();
-    app.getGroupHelper().fillGroupForm(new GroupData("test 1", "test header logo", "test footer comment"));
+    app.getGroupHelper().fillGroupForm(new GroupData("test 1", null, null));
     app.getGroupHelper().submitGroupCreation();
     app.getNavigationHelper().gotoGroupPage();
     app.getSessionHelper().logout();
