4d175fc0ea9e0681698861bf5d35efb6f7b5f4d6
wematall <wematall@gmail.com>
2019-06-12 18:44:55 +0300

    Добавил проверку что контакт существует и если нет предварительно создается

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -8,6 +8,12 @@ public class ContactModificationTests extends TestBase {
 	@Test
 	public void testContactModification () {
 		app.getNavigationHelper().goToContactPage();
+
+		if (! app.getContactHelper().isThereContact()) {
+			app.getContactHelper().createContact(new ContactData("testMiddleName", "testLastName", "testNickname", "mr", "testCompany", "testCity, st. testStreet, 11", "11111111111", "22222222222", "22222222222", "33333333333", "testmail@mail.test", "testFirstName", "testmai2@mail.test", "testmail3@mail.test", "www.somewebpage.test", "7", "August", "1988", "11", "December", "2011", "test 1","testCity2, st. testStreet, 111", "internet virtual home", "some notes, some comments here"), true);
+			app.getNavigationHelper().goToContactPage();
+		}
+
 		app.getContactHelper().selectContact();
 		app.getContactHelper().initContactModification();
 		app.getContactHelper().fillContactForm(new ContactData("testNewMiddleName", "testNewLastName", "testNewNickname", "mr", "testCompany", "testCity, st. testStreet, 11", "11111111111", "22222222222", "22222222222", "33333333333", "testmail@mail.test", "testFirstName", "testmai2@mail.test", "testmail3@mail.test", "www.somewebpage.test", "7", "August", "1988", "11", "December", "2011", null,"testCity2, st. testStreet, 111", "internet virtual home", "some notes, some comments here"), false);
