99c2b6766025c3c16bea0ea373f46a12d3ddb3bb
wematall <wematall@gmail.com>
2019-06-29 13:35:03 +0300

    изменил аннотации тестовых методов и модификатор статик

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -2,23 +2,23 @@ package ru.stqa.pft.addressbook.tests;
 
 
 import org.openqa.selenium.remote.BrowserType;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
 
 
 
 public class TestBase {
 
-	public ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
+	public static ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
 
-	@BeforeMethod(alwaysRun = true)
+	@BeforeSuite
 	public void setUp() throws Exception {
 		app.init();
 	}
 
 
-	@AfterMethod(alwaysRun = true)
+	@AfterSuite
 	public void tearDown() throws Exception {
 		app.stop();
 	}
